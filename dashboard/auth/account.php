94c64ebf33639be8ce8a5501fa6c68e594db8042
ThankGod Okoro <thankgodokoro@ThankGods-MBP.lan>
2022-10-10 20:17:25 +0100

    user modification

--- dashboard/auth/account.php
+++ dashboard/auth/account.php
@@ -90,6 +90,8 @@ if (isset($_POST['login_btn'])) {
         echo "<meta http-equiv='refresh' content='5; URL=./'>";
     }elseif ($status == 'Active') {
         header('location: dashboard');
+    }elseif ($verified == 1) {
+        header('location: dashboard');
     }else {
         $_SESSION['error_message'] = "Incorrect Login Details";
         echo "<meta http-equiv='refresh' content='5; URL=./'>";
@@ -127,37 +129,37 @@ if(isset($_POST['kyc_btn'])) {
 
 
 //Upload Profile Picture
-if (isset($_POST['picture_btn'])) {
+// if (isset($_POST['picture_btn'])) {
 
-    $id = $_SESSION['id'];
-    $id = $conn->real_escape_string($_POST['id']);
-    $picture_path  = $conn->real_escape_string('./upload/'.$_FILES['picture']['name']);
+//     $id = $_SESSION['id'];
+//     $id = $conn->real_escape_string($_POST['id']);
+//     $picture_path  = $conn->real_escape_string('./upload/'.$_FILES['picture']['name']);
 
-    if (file_exists($picture_path)){
-        $picture_path = $conn->real_escape_string('./upload/'.uniqid().rand().$_FILES['picture']['name']);
-    }
+//     if (file_exists($picture_path)){
+//         $picture_path = $conn->real_escape_string('./upload/'.uniqid().rand().$_FILES['picture']['name']);
+//     }
 
-    $checker = 0;
+//     $checker = 0;
 
-    //make sure file type is image
-    if (preg_match("!image!", $_FILES['picture']['type'])) {
-        $checker ++;
-    }
-    if ($checker < 1){
-        exit;
-    }
+//     //make sure file type is image
+//     if (preg_match("!image!", $_FILES['picture']['type'])) {
+//         $checker ++;
+//     }
+//     if ($checker < 1){
+//         exit;
+//     }
 
-    $sql=mysqli_query($conn,"SELECT * FROM users where id='".$_SESSION['id']."'");
-    $num=mysqli_fetch_array($sql);
-    if($num>0){
-        $conn=mysqli_query($conn,"UPDATE users SET picture='$picture_path'  where id='".$_SESSION['id']."'");
+//     $sql=mysqli_query($conn,"SELECT * FROM users where id='".$_SESSION['id']."'");
+//     $num=mysqli_fetch_array($sql);
+//     if($num>0){
+//         $conn=mysqli_query($conn,"UPDATE users SET picture='$picture_path'  where id='".$_SESSION['id']."'");
 
-        //copy image to upload folder
-        copy($_FILES['picture']['tmp_name'], $picture_path);
+//         //copy image to upload folder
+//         copy($_FILES['picture']['tmp_name'], $picture_path);
 
-        $_SESSION['success_message'] = "Picture uploaded 👍";
-    }
-    else{
-        $_SESSION['error_message'] = "Error uploading photograph.";
-    }
-}
+//         $_SESSION['success_message'] = "Picture uploaded 👍";
+//     }
+//     else{
+//         $_SESSION['error_message'] = "Error uploading photograph.";
+//     }
+// }
