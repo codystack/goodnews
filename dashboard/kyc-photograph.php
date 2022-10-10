4505c29a45e2c3168d6be60b9491e7b8ab3aed59
ThankGod Okoro <thankgodokoro@ThankGods-MBP.lan>
2022-10-10 19:55:50 +0100

    user modification

--- dashboard/kyc-photograph.php
+++ dashboard/kyc-photograph.php
@@ -166,7 +166,7 @@ require_once "./auth/picture.php";
                         </form>
                     </div>
 
-                    <div class="card-body bg-white text-center <?php if(empty($_SESSION['picture'])){echo 'd-none';}else{ echo'd-unset';}?>">
+                    <div class="card-body bg-white text-center <?php if(empty(['picture'])){echo 'd-none';}else{ echo'd-unset';}?>">
                         <div class="checkmark mt-2">
                             <svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
                                     viewBox="0 0 161.2 161.2" enable-background="new 0 0 161.2 161.2" xml:space="preserve">
